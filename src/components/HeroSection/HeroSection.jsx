2cc1c09f73415263aec51236a0e6799854160800
Mhr946200 <hasancoders0@gmail.com>
2026-04-01 05:35:31 +0600

    Hero UI updated

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const HeroSection = () => {
     return (
-        <div>
-            <h1>Hero Section</h1>
-        </div>
+        <>
+            <div className='container m-auto px-5'>
+                <div className="hero min-h-screen">
+                    <div className="hero-content flex-col lg:flex-row-reverse">
+                        <img src="./src/assets/banner.png"className="max-w-sm"/>
+                        <div>
+                        <div className='bg-primary/10 w-fit px-6 py-2 rounded-full text-primary font-medium flex gap-2'>
+                            <img className='w-6' src='/src/assets/indicator.png'></img>
+                            <p>New: AI-Powered Tools Available</p>
+                        </div>
+                        <h1 className="text-6xl font-bold leading-18 mt-3">Supercharge Your Digital Workflow!</h1>
+                        <p className="py-5 text-gray-500">
+                            Access premium AI tools, design assets, templates, and productivity
+                            software—all in one place. Start creating faster today.
+                            Explore Products
+                        </p>
+                        <button className="btn btn-primary rounded-full mr-3">Get Started</button>
+                        <button className="btn btn-outline btn-primary rounded-full"><img src='/src/assets/Play.png'></img>Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
