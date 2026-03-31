fa7032215d4f8d6f035b8bc8267dc4977f9eb231
Mhr946200 <hasancoders0@gmail.com>
2026-04-01 04:58:09 +0600

    Header UI done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,48 @@
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+<div className="bg-base-100 shadow-sm">
+    <div className="container navbar m-auto">
+        <div className="navbar-start">
+            <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                <li><a>Products</a></li>
+                <li><a>Features</a></li>
+                <li><a>Pricing</a></li>
+                <li><a>Testimonials</a></li>
+                <li><a>FAQ</a></li>
+            </ul>
+            </div>
+            <a className="btn btn-ghost text-3xl text-primary font-bold">DigiTools</a>
         </div>
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+                <li><a>Products</a></li>
+                <li><a>Features</a></li>
+                <li><a>Pricing</a></li>
+                <li><a>Testimonials</a></li>
+                <li><a>FAQ</a></li>
+            </ul>
+        </div>
+        <div className="navbar-end">
+            <div role="button" className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+                    <span className="badge badge-sm indicator-item">8</span>
+                </div>
+            </div>
+            <a className="link link-hover ml-2">Login</a>
+            <a className="btn btn-active btn-primary mx-2 rounded-full">Get Started</a>
+        </div>
+    </div>
+</div>
+
+        
     );
 };
 
